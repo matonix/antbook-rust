73abf2eb48276667ef525e139b1d14bf6e8b4663
matonix <automatonix@gmail.com>
2022-06-01 23:43:12 +0900

    3_3 segtree crane

--- src/bin/3_3_crane.rs
+++ src/bin/3_3_crane.rs
@@ -1,31 +1,82 @@
-use proconio::input;
-use itertools::Itertools;
+use antbook::segtree::{Monoid, SegTree};
 use itertools::izip;
+use itertools::Itertools;
+use num::Complex;
+use proconio::input;
 use std::f64::consts::PI;
 
 fn main() {
   input! {
-    n: usize, 
-    c: usize, 
+    n: usize,
+    c: usize,
     l: [usize; n],
-    s: [usize; c], 
+    s: [usize; c],
     a: [usize; n]
   }
   println!("{:?}", solve(n, c, l, s, a));
 }
 
+// クエリ a, b に対しては a..b のベクトル和を返し、 更新 i, x に対しては要素 i のベクトルを x にする（ベクトルの表現は複素数で持つ）
+fn solve(_n: usize, _c: usize, l: Vec<usize>, s: Vec<usize>, a: Vec<usize>) -> Vec<(f64, f64)> {
+  let z = l.iter().map(|&len| Node::new(0.0, Complex::new(0.0, len as f64))).collect_vec(); // ベクトルを表す複素数として初期化 (Im 軸沿い)
+  let mut st = SegTree::new(z);
+  let a = a.iter().map(|&deg| deg as f64 / 180.0 * PI - PI).collect_vec(); // 前のベクトルからどれだけ回転するか（前のベクトルと同じならゼロ）
+  let mut ans = vec![];
+  fn query(st: &mut SegTree<Node>, s: usize, a: f64) -> (f64, f64) {
+    let n = st.query(s, s + 1); // 更新対象
+    st.update(s, Node::new(a, Complex::from_polar(1.0, a) * n.z));
+    let n = st.query(0, st.size());
+    (n.z.re, n.z.im)
+  }
+  for (s, a) in izip!(s, a) {
+    ans.push(query(&mut st, s, a));
+  }
+  ans
+}
+
+// モノイドを構成する解法: https://minus3theta.hatenablog.com/entry/2020/04/02/014949
+// 和ベクトルの次のベクトルが刺さる向き θ と和ベクトル z の組
+#[derive(Debug, Clone, PartialEq)]
+pub struct Node {
+  theta: f64,
+  z: Complex<f64>,
+}
+
+impl Monoid for Node {
+  fn op(a: Self, b: Self) -> Self {
+    Self {
+      theta: a.theta + b.theta,
+      z: a.z + Complex::from_polar(1.0, a.theta) * b.z,
+    }
+  }
+  fn e() -> Self {
+    Self {
+      theta: 0.0,
+      z: Complex::new(0.0, 0.0),
+    }
+  }
+}
+
+impl Node {
+  fn new(theta: f64, z: Complex<f64>) -> Self {
+    Self { theta, z }
+  }
+}
+
 // ラジアン極座標系 (r, θ) を扱う
 // 愚直実装 O(NC)
-fn solve(n: usize, _c: usize, l: Vec<usize>, s: Vec<usize>, a: Vec<usize>) -> Vec<(f64, f64)> {
+fn _solve(n: usize, _c: usize, l: Vec<usize>, s: Vec<usize>, a: Vec<usize>) -> Vec<(f64, f64)> {
   let r = l.iter().map(|&len| len as f64).collect_vec();
-  let mut t = vec![PI/2.0; n];
-  let a = a.iter().map(|&deg| deg as f64 / 180.0 * PI).collect_vec();
-  let s = s.iter().map(|&idx| idx - 1).collect_vec();
+  let mut t = vec![PI / 2.0; n];
+  let a = a.iter().map(|&deg| deg as f64 / 180.0 * PI).collect_vec(); // ラジアンへの変換
+  let s = s.iter().map(|&idx| idx - 1).collect_vec(); // 0-indexed
   let mut ans = vec![];
   fn query(r: &Vec<f64>, t: &mut Vec<f64>, s: usize, a: f64) -> (f64, f64) {
-    let rot = a + t[s] - PI - t[s+1]; // 回転後を θ'_i+1 とすると、 a_i = π - θ_i + θ'_i+1 となる。ここで求めたいのは変化量 θ'_i+1 - θ_i+1 
-    t[s+1..].iter_mut().for_each(|rad| *rad += rot);
-    izip![r, t].fold((0.0, 0.0), |(x, y), (r, t)| (x + r * t.cos(), y + r * t.sin()))
+    let rot = a + t[s] - PI - t[s + 1]; // 回転後を θ'_i+1 とすると、 a_i = π - θ_i + θ'_i+1 となる。ここで求めたいのは変化量 θ'_i+1 - θ_i+1
+    t[s + 1..].iter_mut().for_each(|rad| *rad += rot);
+    izip![r, t].fold((0.0, 0.0), |(x, y), (r, t)| {
+      (x + r * t.cos(), y + r * t.sin())
+    })
   }
   for (s, a) in izip!(s, a) {
     ans.push(query(&r, &mut t, s, a));
@@ -46,7 +97,23 @@ mod tests {
     let l = vec![10, 5];
     let s = vec![1];
     let a = vec![90];
-    for ((x_a, y_a), (x_e, y_e)) in izip!(solve(n, c, l, s, a), vec![(5.00, 10.00)]) {
+    let expect = vec![(5.00, 10.00)];
+    let actual = solve(n, c, l, s, a);
+    for ((x_a, y_a), (x_e, y_e)) in izip!(actual, expect) {
+      assert_approx_eq!(x_a, x_e);
+      assert_approx_eq!(y_a, y_e);
+    }
+  }
+  #[test]
+  fn example2() {
+    let n = 3;
+    let c = 2;
+    let l = vec![5, 5, 5];
+    let s = vec![1, 2];
+    let a = vec![270, 90];
+    let expect = vec![(-10.00, 5.00), (-5.00, 10.00)];
+    let actual = solve(n, c, l, s, a);
+    for ((x_a, y_a), (x_e, y_e)) in izip!(actual, expect) {
       assert_approx_eq!(x_a, x_e);
       assert_approx_eq!(y_a, y_e);
     }
--- src/segtree.rs
+++ src/segtree.rs
@@ -1,24 +1,73 @@
 use itertools::Itertools;
-use num::Bounded;
+use num::{Bounded, Zero};
 use std::cmp::min;
 
-// Segment tree による Range Minimum Query (RMQ) (3-3 節)
+// Segment tree による Range Minimum Query (Min) (3-3 節)
 // s, t が与えられたとき、[s, t] の最小値を求める O(log n)
 // i, x が与えられたとき、[i] = x に変更する O(log n)
 
-#[derive(Debug)]
-pub struct SegTree<T>(Vec<T>);
+// 一般にはモノイド （op, e) 上の構造を持つ
+pub trait Monoid {
+  fn op(a: Self, b: Self) -> Self;
+  fn e() -> Self;
+}
 
-impl<T: Ord + Clone + Bounded> SegTree<T> {
-  pub fn rmq(vec: Vec<T>) -> Self {
-    fn go<T: Ord + Clone + Bounded>(vec: Vec<T>, acc: &mut Vec<Vec<T>>) {
+#[derive(Debug)]
+pub struct SegTree<T>(Vec<T>, usize);
+
+impl<T> Monoid for T
+where
+  T: Zero,
+{
+  fn op(a: Self, b: Self) -> Self {
+    a.add(b)
+  }
+  fn e() -> Self {
+    T::zero()
+  }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Min<T>(T);
+
+impl<T> Monoid for Min<T>
+where
+  T: Ord + Bounded + Clone,
+{
+  fn op(a: Self, b: Self) -> Self {
+    Self(min(a.0, b.0))
+  }
+  fn e() -> Self {
+    Self(T::max_value())
+  }
+}
+
+impl<T> Min<T>
+where
+  T: Ord + Bounded + Clone,
+{
+  pub fn rmq(vec: Vec<T>) -> SegTree<Min<T>> {
+    let vec = vec.into_iter().map(|v| Min(v)).collect_vec();
+    SegTree::new(vec)
+  }
+}
+
+impl<T> SegTree<T>
+where
+  T: Clone + Monoid,
+{
+  pub fn new(vec: Vec<T>) -> Self {
+    fn go<T>(vec: Vec<T>, acc: &mut Vec<Vec<T>>)
+    where
+      T: Clone + Monoid,
+    {
       if vec.len() <= 1 {
         acc.push(vec);
       } else {
         acc.push(vec.clone());
         let mut new_vec = vec![];
         for (l, r) in vec.iter().tuples() {
-          new_vec.push(min(l, r).clone());
+          new_vec.push(T::op(l.clone(), r.clone()));
         }
         if vec.len() % 2 != 0 {
           if let Some(last) = vec.last() {
@@ -29,43 +78,40 @@ impl<T: Ord + Clone + Bounded> SegTree<T> {
       }
     }
     // padding to 2^n
+    let len = vec.len();
     let mut n = 1;
-    while vec.len() > n {
+    while len > n {
       n = n << 1;
     }
-    let pad = n - vec.len();
-    let vec = vec![vec, vec![T::max_value(); pad]].concat();
+    let pad = n - len;
+    let vec = vec![vec, vec![T::e(); pad]].concat();
     let mut acc = vec![];
     go(vec, &mut acc);
     acc.reverse();
-    SegTree(acc.concat())
+    SegTree(acc.concat(), len)
   }
 
   // [s, t) の最小値を求める
   // 見つからない場合は MaxBound を返す
   pub fn query(&self, s: usize, t: usize) -> T {
     // k は接点の番号であり、 [l, r) に対応する
-    fn go<T: Ord + Clone + Bounded>(
-      v: &Vec<T>,
-      s: usize,
-      t: usize,
-      k: usize,
-      l: usize,
-      r: usize,
-    ) -> T {
+    fn go<T>(st: &SegTree<T>, s: usize, t: usize, k: usize, l: usize, r: usize) -> T
+    where
+      T: Clone + Monoid,
+    {
       // [s, t), [l, r) に共通部分がない場合は終了
       if r <= s || t <= l {
-        T::max_value()
+        T::e()
         // [s, t) が [l, r) を包摂する場合はノードの値を返す
       } else if s <= l && r <= t {
-        v[k].clone()
+        st.0[k].clone()
       } else {
-        let vl = go(v, s, t, k * 2 + 1, l, (l + r) / 2);
-        let vr = go(v, s, t, k * 2 + 2, (l + r) / 2, r);
-        min(vl, vr)
+        let vl = go(st, s, t, k * 2 + 1, l, (l + r) / 2);
+        let vr = go(st, s, t, k * 2 + 2, (l + r) / 2, r);
+        T::op(vl, vr)
       }
     }
-    go(&self.0, s, t, 0, 0, (self.0.len() + 1) / 2)
+    go(&self, s, t, 0, 0, (self.0.len() + 1) / 2)
   }
 
   pub fn update(&mut self, i: usize, x: T) {
@@ -75,8 +121,12 @@ impl<T: Ord + Clone + Bounded> SegTree<T> {
     // 登りながら更新
     while k > 0 {
       k = (k - 1) / 2;
-      self.0[k] = min(self.0[k * 2 + 1].clone(), self.0[k * 2 + 2].clone());
+      self.0[k] = T::op(self.0[k * 2 + 1].clone(), self.0[k * 2 + 2].clone());
     }
+  }
+
+  pub fn size(&self) -> usize {
+    self.1
   }
 }
 
@@ -87,27 +137,27 @@ mod tests {
   #[test]
   fn sample_query() {
     let v = vec![5, 3, 7, 9, 6, 4, 1, 2];
-    let st = SegTree::rmq(v);
-    assert_eq!(st.query(0, 7), 1);
+    let st = Min::<usize>::rmq(v);
+    assert_eq!(st.query(0, 7).0, 1);
   }
   #[test]
   fn sample_update_query() {
     let v = vec![5, 3, 7, 9, 6, 4, 1, 2];
-    let mut st = SegTree::rmq(v);
-    st.update(0, 2);
-    assert_eq!(st.query(0, 5), 2);
+    let mut st = Min::<usize>::rmq(v);
+    st.update(0, Min(2));
+    assert_eq!(st.query(0, 5).0, 2);
   }
   #[test]
   fn sample_query_odd() {
     let v = vec![5, 3, 7, 9, 6, 4, 1];
-    let st = SegTree::rmq(v);
-    assert_eq!(st.query(0, 6), 3);
+    let st = Min::<usize>::rmq(v);
+    assert_eq!(st.query(0, 6).0, 3);
   }
   #[test]
   fn sample_update_query_odd() {
     let v = vec![5, 3, 7, 9, 6, 4, 1];
-    let mut st = SegTree::rmq(v);
-    st.update(0, 2);
-    assert_eq!(st.query(0, 5), 2);
+    let mut st = Min::<usize>::rmq(v);
+    st.update(0, Min(2));
+    assert_eq!(st.query(0, 5).0, 2);
   }
 }
