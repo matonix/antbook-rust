496f865510384ba21b86bc4227b77d4ad327117b
matonix <automatonix@gmail.com>
2022-06-11 12:50:41 +0900

    rename type variable

--- src/segtree.rs
+++ src/segtree.rs
@@ -6,60 +6,23 @@ use std::cmp::min;
 // s, t が与えられたとき、[s, t] の最小値を求める O(log n)
 // i, x が与えられたとき、[i] = x に変更する O(log n)
 
-// 一般にはモノイド （op, e) 上の構造を持つ
+// 一般にはモノイド （X, op, e) 上の構造を持つ
 pub trait Monoid {
   fn op(a: &Self, b: &Self) -> Self;
   fn e() -> Self;
 }
 
 #[derive(Debug)]
-pub struct SegTree<T>(Vec<T>, usize);
+pub struct SegTree<X>(Vec<X>, usize);
 
-impl<T> Monoid for T
+impl<X> SegTree<X>
 where
-  T: Zero + Clone,
+  X: Clone + Monoid,
 {
-  fn op(a: &Self, b: &Self) -> Self {
-    a.clone() + b.clone()
-  }
-  fn e() -> Self {
-    T::zero()
-  }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Min<T>(T);
-
-impl<T> Monoid for Min<T>
-where
-  T: Ord + Bounded + Clone,
-{
-  fn op(a: &Self, b: &Self) -> Self {
-    Self(min(a.0.clone(), b.0.clone()))
-  }
-  fn e() -> Self {
-    Self(T::max_value())
-  }
-}
-
-impl<T> Min<T>
-where
-  T: Ord + Bounded + Clone,
-{
-  pub fn rmq(vec: Vec<T>) -> SegTree<Min<T>> {
-    let vec = vec.into_iter().map(|v| Min(v)).collect_vec();
-    SegTree::new(vec)
-  }
-}
-
-impl<T> SegTree<T>
-where
-  T: Clone + Monoid,
-{
-  pub fn new(vec: Vec<T>) -> Self {
-    fn go<T>(vec: Vec<T>, acc: &mut Vec<Vec<T>>)
+  pub fn new(vec: Vec<X>) -> Self {
+    fn go<X>(vec: Vec<X>, acc: &mut Vec<Vec<X>>)
     where
-      T: Clone + Monoid,
+      X: Clone + Monoid,
     {
       if vec.len() <= 1 {
         acc.push(vec);
@@ -67,7 +30,7 @@ where
         acc.push(vec.clone());
         let mut new_vec = vec![];
         for (l, r) in vec.iter().tuples() {
-          new_vec.push(T::op(l, r));
+          new_vec.push(X::op(l, r));
         }
         if vec.len() % 2 != 0 {
           if let Some(last) = vec.last() {
@@ -84,7 +47,7 @@ where
       n = n << 1;
     }
     let pad = n - len;
-    let vec = vec![vec, vec![T::e(); pad]].concat();
+    let vec = vec![vec, vec![X::e(); pad]].concat();
     let mut acc = vec![];
     go(vec, &mut acc);
     acc.reverse();
@@ -93,34 +56,34 @@ where
 
   // [s, t) の最小値を求める
   // 見つからない場合は MaxBound を返す
-  pub fn query(&self, s: usize, t: usize) -> T {
+  pub fn query(&self, s: usize, t: usize) -> X {
     // k は接点の番号であり、 [l, r) に対応する
-    fn go<T>(st: &SegTree<T>, s: usize, t: usize, k: usize, l: usize, r: usize) -> T
+    fn go<X>(st: &SegTree<X>, s: usize, t: usize, k: usize, l: usize, r: usize) -> X
     where
-      T: Clone + Monoid,
+      X: Clone + Monoid,
     {
       // [s, t), [l, r) に共通部分がない場合は終了
       if r <= s || t <= l {
-        T::e()
+        X::e()
         // [s, t) が [l, r) を包摂する場合はノードの値を返す
       } else if s <= l && r <= t {
         st.0[k].clone()
       } else {
         let vl = go(st, s, t, k * 2 + 1, l, (l + r) / 2);
         let vr = go(st, s, t, k * 2 + 2, (l + r) / 2, r);
-        T::op(&vl, &vr)
+        X::op(&vl, &vr)
       }
     }
     go(&self, s, t, 0, 0, (self.0.len() + 1) / 2)
   }
 
-  pub fn update(&mut self, i: usize, x: T) {
+  pub fn update(&mut self, i: usize, x: X) {
     self.update_by(i, |_| x.clone())
   }
 
   pub fn update_by<F>(&mut self, i: usize, mut f: F)
   where
-    F: FnMut(&T) -> T,
+    F: FnMut(&X) -> X,
   {
     // 葉の接点
     let mut k = i + (self.0.len() + 1) / 2 - 1;
@@ -128,12 +91,56 @@ where
     // 登りながら更新
     while k > 0 {
       k = (k - 1) / 2;
-      self.0[k] = T::op(&self.0[k * 2 + 1], &self.0[k * 2 + 2]);
+      self.0[k] = X::op(&self.0[k * 2 + 1], &self.0[k * 2 + 2]);
     }
   }
 
   pub fn size(&self) -> usize {
     self.1
+  }
+}
+
+// 具体的なモノイド
+
+// (X, +, 0)
+// プリミティブ型向け
+impl<X> Monoid for X
+where
+  X: Zero + Copy, // Zero は Add 上に定義されている
+{
+  fn op(a: &Self, b: &Self) -> Self {
+    a.add(*b)
+  }
+  fn e() -> Self {
+    X::zero()
+  }
+}
+
+// Range Minimum Query
+// (M∪{∞}, min, ∞)
+#[derive(Debug, Clone, PartialEq)]
+pub struct Min<X>(X);
+
+impl<X> Monoid for Min<X>
+where
+  X: Ord + Bounded + Clone,
+{
+  fn op(a: &Self, b: &Self) -> Self {
+    Self(min(a.0.clone(), b.0.clone()))
+  }
+  fn e() -> Self {
+    Self(X::max_value())
+  }
+}
+
+// RMQ 向け生成関数
+impl<X> Min<X>
+where
+  X: Ord + Bounded + Clone,
+{
+  pub fn rmq(vec: Vec<X>) -> SegTree<Min<X>> {
+    let vec = vec.into_iter().map(|v| Min(v)).collect_vec();
+    SegTree::new(vec)
   }
 }
 
