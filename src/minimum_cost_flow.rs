8b0399cb541afa5738f13273a812ae4e84293c33
matonix <automatonix@gmail.com>
2022-11-10 23:32:37 +0900

    WIP SSP w/ dijkstra

--- src/minimum_cost_flow.rs
+++ src/minimum_cost_flow.rs
@@ -1,14 +1,15 @@
-use crate::shortest_path::bellman_ford;
+use crate::{shortest_path::bellman_ford, dijkstra, dijkstra_with_potential, dot_log};
 use num::{traits::{NumAssign, WrappingAdd}, Bounded};
 use petgraph::prelude::*;
-use std::{collections::HashMap, fmt::Debug};
+use std::collections::HashMap;
+use std::fmt::Debug;
 
 // paper: https://arxiv.org/pdf/1207.6381.pdf
 
 // successive shortest path with bellman-ford                    cap cost
-pub fn ssp_bf<E>(s: NodeIndex, t: NodeIndex, graph: &DiGraph<(), (E, E)>, flow_obj: E) -> Option<E>
+pub fn ssp_bf<W>(s: NodeIndex, t: NodeIndex, graph: &DiGraph<(), (W, W)>, flow_obj: W) -> Option<W>
 where
-  E: Copy + NumAssign + Bounded + Ord + Debug + WrappingAdd,
+  W: Copy + NumAssign + Bounded + Ord + Debug + WrappingAdd,
 {
   // 初期残余グラフ
   let mut res = graph.clone();
@@ -17,13 +18,13 @@ where
     let rev_e = res.add_edge(
       e.target(),
       e.source(),
-      (E::zero(), E::zero() - e.weight().1),
+      (W::zero(), W::zero() - e.weight().1),
     );
     revs.insert(e.id(), rev_e);
     revs.insert(rev_e, e.id());
   }
-  let mut cost = E::zero();
-  let mut flow_tot = E::zero();
+  let mut cost = W::zero();
+  let mut flow_tot = W::zero();
   while flow_tot < flow_obj {
     // Bellman-ford で残余グラフ上の s-t 最短路を求める
     let g = res.filter_map(
@@ -32,7 +33,7 @@ where
     ); // 探索用コストグラフ
     if let Ok(paths) = bellman_ford(&g, s) {
       // s-t 最短路に沿って流量を決定
-      let mut f = E::max_value();
+      let mut f = W::max_value();
       let mut curr = t;
       let mut path = vec![];
       while let Some(pred) = paths.1[curr.index()] {
@@ -66,6 +67,83 @@ where
   Some(cost)
 }
 
+// successive shortest path with dijkstra                       cap cost
+pub fn ssp_di<W>(s: NodeIndex, t: NodeIndex, graph: &DiGraph<(), (W, W)>, flow_obj: W) -> Option<W>
+where
+  W: Copy + NumAssign + Bounded + Ord + Debug + WrappingAdd,
+{
+  // // 初期ポテンシャル w/ dijkstra
+  // let g = graph.filter_map(
+  //   |_, n| Some(n),
+  //   |_, e| if e.0.is_zero() { None } else { Some(e.1) },
+  // ); // 探索用コストグラフ
+  // let potentials = dijkstra(&g, s).0;
+
+  // 初期残余グラフ
+  let mut res = graph.map(|_, _| W::zero(), |_, e| *e);
+  let mut revs = HashMap::new();
+  for e in graph.edge_references() {
+    let rev_e = res.add_edge(
+      e.target(),
+      e.source(),
+      (W::zero(), W::zero() - e.weight().1),
+    );
+    revs.insert(e.id(), rev_e);
+    revs.insert(rev_e, e.id());
+  }
+
+  dot_log::log(&res, 0);
+
+  let mut cost = W::zero();
+  let mut flow_tot = W::zero();
+  while flow_tot < flow_obj {
+    dot_log::log(&res, 1);
+    // dijkstra で残余グラフ上の s-t 最短路を求める
+    let g = res.filter_map(
+      |_, n| Some(*n),
+      |_, e| if e.0.is_zero() { None } else { Some(e.1) },
+    ); // 探索用コストグラフ
+    let paths = dijkstra_with_potential(&g, s);
+    // ポテンシャルの更新
+    for (i, w) in res.node_weights_mut().enumerate() {
+      *w = paths.0[i]
+    }
+    // s-t 最短路に沿って流量を決定
+    let mut f = W::max_value();
+    let mut curr = t;
+    let mut path = vec![];
+    while let Some(pred) = paths.1[curr.index()] {
+      let cap = res
+        .find_edge(pred, curr)
+        .and_then(|e| {
+          path.push(e);
+          res.edge_weight(e)
+        })
+        .map_or(f, |w| w.0);
+      f = f.min(cap);
+      curr = pred;
+    }
+    if curr != s {
+      // s-t パスが存在しない
+      return None;
+    }
+    f = f.min(flow_obj - flow_tot); // 流しすぎないように
+    flow_tot += f;
+    cost += paths.0[t.index()] * f;
+    // 残余グラフを更新
+    for e in path.into_iter() {
+      res.edge_weight_mut(e).unwrap().0 -= f;
+      res.edge_weight_mut(revs[&e]).unwrap().0 += f;
+      dot_log::log(&res, 2);
+    }
+    dot_log::log(&res, 1);
+  }
+  dot_log::log(&res, 0);
+  dot_log::write_dot();
+  Some(cost)
+}
+
+
 mod tests {
   #[test]
   fn test() {
@@ -88,5 +166,9 @@ mod tests {
     assert_eq!(super::ssp_bf(s, t, &graph, 5), Some(39));
     assert_eq!(super::ssp_bf(s, t, &graph, 11), Some(102));
     assert_eq!(super::ssp_bf(s, t, &graph, 12), None);
+    // assert_eq!(super::ssp_di(s, t, &graph, 0), Some(0));
+    assert_eq!(super::ssp_di(s, t, &graph, 5), Some(39));
+    // assert_eq!(super::ssp_di(s, t, &graph, 11), Some(102));
+    // assert_eq!(super::ssp_di(s, t, &graph, 12), None);
   }
 }
--- src/shortest_path.rs
+++ src/shortest_path.rs
@@ -1,62 +1,150 @@
-// Num系トレイトを使った辺の重さの制約 2_5_bellman_ford_num.rs
 use num::*;
 use num::traits::WrappingAdd;
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+use std::collections::HashMap;
+use std::hash::Hash;
 use petgraph::prelude::*;
 use petgraph::visit::*;
 
-mod tests {
+// 経路復元
+pub fn get_path<G>(graph: G, prev: Vec<Option<G::NodeId>>, goal: G::NodeId) -> Vec<G::NodeId>
+where
+  G: NodeIndexable,
+  G::NodeId: Eq,
+{
+  let mut curr = goal;
+  let mut path = vec![goal];
+  let ix = |x| graph.to_index(x);
+  while let Some(prev) = prev[ix(curr)] {
+    path.push(prev);
+    curr = prev;
+  }
+  path.reverse();
+  path
+}
 
-  #[allow(unused_variables)]
-  #[test]
-  fn test() {
-    let mut g = super::Graph::new();
-    let a = g.add_node(()); // node with no weight
-    let b = g.add_node(());
-    let c = g.add_node(());
-    let d = g.add_node(());
-    let e = g.add_node(());
-    let f = g.add_node(());
-    g.extend_with_edges(&[
-      (0, 1, 2),
-      (0, 3, 4),
-      (1, 2, 1),
-      (1, 5, 7),
-      (2, 4, 5),
-      (4, 5, 1),
-      (3, 4, 1),
-    ]);
+// 蟻本の実装
+// use petgraph::algo::dijkstra; で一発だが、経路復元のための構造を返す。
+pub fn dijkstra<G>(
+  graph: G,
+  start: G::NodeId,
+) -> (Vec<G::EdgeWeight>, Vec<Option<G::NodeId>>)
+where
+  G: IntoEdges + Visitable + NodeIndexable,
+  G::NodeId: Eq,
+  G::EdgeWeight: Copy + Ord + Num + Bounded,
+{
+  let mut visited = graph.visit_map();
+  let mut scores = vec![<G::EdgeWeight>::max_value(); graph.node_bound()];
+  let mut predecessor = vec![None; graph.node_bound()];
+  let mut visit_next = BinaryHeap::new();
+  let ix = |x| graph.to_index(x);
+  scores[graph.to_index(start)] = <G::EdgeWeight>::zero();
+  visit_next.push(MinScored(<G::EdgeWeight>::zero(), start));
 
-    // Graph represented with the weight of each edge
-    //
-    //     2       1
-    // a ----- b ----- c
-    // | 4     | 7     |
-    // d       f       | 5
-    // | 1     | 1     |
-    // \------ e ------/
+  while let Some(MinScored(score, node)) = visit_next.pop() {
+    if visited.is_visited(&node) {
+      continue;
+    }
+    for edge in graph.edges(node) {
+      let next = edge.target();
+      if visited.is_visited(&next) {
+        continue;
+      }
+      let next_score = score + *edge.weight();
+      scores[ix(next)] = std::cmp::min(scores[ix(next)], next_score);
+      visit_next.push(MinScored(scores[ix(next)], next));
+      predecessor[ix(next)] = Some(node);
+    }
+    visited.visit(node);
+  }
+  (scores, predecessor)
+}
 
-    let path = super::bellman_ford(&g, a);
-    assert_eq!(
-      path,
-      Ok((
-        vec![0, 2, 3, 4, 5, 6],
-        vec![None, Some(a), Some(b), Some(a), Some(d), Some(e)]
-      ))
-    );
-    let graph_with_neg_cycle = super::UnGraph::<(), i32>::from_edges(&[
-      (0, 1, -2),
-      (0, 3, -4),
-      (1, 2, -1),
-      (1, 5, -25),
-      (2, 4, -5),
-      (4, 5, -25),
-      (3, 4, -1),
-    ]);
+pub fn dijkstra_with_potential<G, W>(
+  graph: G,
+  start: G::NodeId,
+) -> (Vec<G::EdgeWeight>, Vec<Option<G::NodeId>>)
+where
+  W: Copy + Ord + Num + Bounded,
+  G: IntoEdges + Visitable + NodeIndexable + IntoNodeReferences<NodeWeight = W, EdgeWeight = W>,
+  G::NodeId: Eq + Hash,
+{
+  let mut visited = graph.visit_map();
+  let mut scores = vec![<G::EdgeWeight>::max_value(); graph.node_bound()];
+  let mut predecessor = vec![None; graph.node_bound()];
+  let mut visit_next = BinaryHeap::new();
+  let ix = |x| graph.to_index(x);
+  scores[graph.to_index(start)] = <G::EdgeWeight>::zero();
+  let node_weights: HashMap<G::NodeId, G::NodeWeight> = graph.node_references().map(|n| (n.id(), *n.weight())).collect();
+  visit_next.push(MinScored(<G::EdgeWeight>::zero() + *node_weights.get(&start).unwrap(), start));
 
-    assert!(super::bellman_ford(&graph_with_neg_cycle, super::NodeIndex::new(0)).is_err());
+  while let Some(MinScored(score, node)) = visit_next.pop() {
+    if visited.is_visited(&node) {
+      continue;
+    }
+    for edge in graph.edges(node) {
+      let next = edge.target();
+      if visited.is_visited(&next) {
+        continue;
+      }
+      let next_score = score + *edge.weight() + *node_weights.get(&node).unwrap() - *node_weights.get(&next).unwrap();
+      scores[ix(next)] = std::cmp::min(scores[ix(next)], next_score);
+      visit_next.push(MinScored(scores[ix(next)], next));
+      predecessor[ix(next)] = Some(node);
+    }
+    visited.visit(node);
+  }
+  (scores, predecessor)
+}
+
+// ヒープに入れるコンテナ
+// https://github.com/petgraph/petgraph/blob/944057cdc57824f90a36f838f3ee7f148157c801/src/scored.rs
+#[derive(Copy, Clone, Debug)]
+pub struct MinScored<K, T>(pub K, pub T);
+
+impl<K: PartialOrd, T> PartialEq for MinScored<K, T> {
+  #[inline]
+  fn eq(&self, other: &MinScored<K, T>) -> bool {
+    self.cmp(other) == Ordering::Equal
   }
 }
 
+impl<K: PartialOrd, T> Eq for MinScored<K, T> {}
+
+impl<K: PartialOrd, T> PartialOrd for MinScored<K, T> {
+  #[inline]
+  fn partial_cmp(&self, other: &MinScored<K, T>) -> Option<Ordering> {
+    Some(self.cmp(other))
+  }
+}
+
+impl<K: PartialOrd, T> Ord for MinScored<K, T> {
+  #[inline]
+  fn cmp(&self, other: &MinScored<K, T>) -> Ordering {
+    let a = &self.0;
+    let b = &other.0;
+    if a == b {
+      Ordering::Equal
+    } else if a < b {
+      Ordering::Greater
+    } else if a > b {
+      Ordering::Less
+    } else if a.ne(a) && b.ne(b) {
+      // these are the NaN cases
+      Ordering::Equal
+    } else if a.ne(a) {
+      // Order NaN less, so that it is last in the MinScore order
+      Ordering::Less
+    } else {
+      Ordering::Greater
+    }
+  }
+}
+
+// Num系トレイトを使った整数重みの辺に対する bellman ford
+// 浮動小数点数の場合は petgraph::algo::bellman_ford
 pub fn bellman_ford<G>(
   g: G,
   source: G::NodeId,
@@ -107,3 +195,89 @@ where
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct NegativeCycle(());
+
+
+mod tests {
+
+  #[allow(unused_variables)]
+  #[test]
+  fn test_dijkstra() {
+    let mut graph = super::Graph::new_undirected();
+    let a = graph.add_node(());
+    let b = graph.add_node(());
+    let c = graph.add_node(());
+    let d = graph.add_node(());
+    let e = graph.add_node(());
+    let f = graph.add_node(());
+    let g = graph.add_node(());
+  
+    // 蟻本の例
+    graph.extend_with_edges(&[
+      (a, b, 2),
+      (a, c, 5),
+      (b, c, 4),
+      (b, d, 6),
+      (b, e, 10),
+      (c, d, 2),
+      (d, f, 1),
+      (e, f, 3),
+      (e, g, 5),
+      (f, g, 9),
+    ]);
+  
+    let (dists, prev) = super::dijkstra(&graph, a);
+    println!("{:?}", dists);
+    println!("{:?}", super::get_path(&graph, prev, g));
+    assert_eq!(dists[petgraph::visit::NodeIndexable::to_index(&graph, d)], 7);
+  }
+
+  #[allow(unused_variables)]
+  #[test]
+  fn test_bellman_ford() {
+    let mut g = super::Graph::new();
+    let a = g.add_node(()); // node with no weight
+    let b = g.add_node(());
+    let c = g.add_node(());
+    let d = g.add_node(());
+    let e = g.add_node(());
+    let f = g.add_node(());
+    g.extend_with_edges(&[
+      (0, 1, 2),
+      (0, 3, 4),
+      (1, 2, 1),
+      (1, 5, 7),
+      (2, 4, 5),
+      (4, 5, 1),
+      (3, 4, 1),
+    ]);
+
+    // Graph represented with the weight of each edge
+    //
+    //     2       1
+    // a ----- b ----- c
+    // | 4     | 7     |
+    // d       f       | 5
+    // | 1     | 1     |
+    // \------ e ------/
+
+    let path = super::bellman_ford(&g, a);
+    assert_eq!(
+      path,
+      Ok((
+        vec![0, 2, 3, 4, 5, 6],
+        vec![None, Some(a), Some(b), Some(a), Some(d), Some(e)]
+      ))
+    );
+    let graph_with_neg_cycle = super::UnGraph::<(), i32>::from_edges(&[
+      (0, 1, -2),
+      (0, 3, -4),
+      (1, 2, -1),
+      (1, 5, -25),
+      (2, 4, -5),
+      (4, 5, -25),
+      (3, 4, -1),
+    ]);
+
+    assert!(super::bellman_ford(&graph_with_neg_cycle, super::NodeIndex::new(0)).is_err());
+  }
+}
